a0ed2ff4930e2c79b1bb052075fea4750435057c
Arnold Su <arnoldtingsu@gmail.com>
2020-10-15 01:43:25 -0700

    10/15/2020

--- _python/python_fundamentals/functions_intermediate2.py
+++ _python/python_fundamentals/functions_intermediate2.py
@@ -21,4 +21,34 @@ students = [
          {'first_name' : 'Mark', 'last_name' : 'Guillen'},
          {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
- 
+
+
+def rewrite(scan_list):
+    for current_dict in scan_list:
+        string_to_print = ""
+        for current_key in current_dict.keys():
+            string_to_print += f"{current_key} -- {current_dict[current_key]}, "
+        string_to_print = string_to_print[:len(string_to_print) -2]
+        print(string_to_print)
+
+rewrite(students)
+
+def get_values(key, current_list):
+    for values in current_list:
+        print(values[key])
+
+get_values('last_name', students)
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def printInfo(current_dict):
+    for values in current_dict.keys():
+        print(len(current_dict[values]), values.upper())
+        for items in current_dict[values]:
+            print(items)
+        print('\n')
+
+printInfo(dojo)
