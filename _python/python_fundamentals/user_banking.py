5c9379260f66983c31c7ca66e985d2750df9ed23
Arnold Su <arnoldtingsu@gmail.com>
2020-10-16 21:32:52 -0700

    10/16/2020

--- _python/python_fundamentals/user_banking.py
+++ _python/python_fundamentals/user_banking.py
@@ -8,15 +8,17 @@ class User:
     def make_deposit(self, amount):
         if amount > self.wallet:
             print("not enough money to deposit")
+            return self
         else : 
             self.account_balance += amount
             self.wallet -= amount
             print(f"{self.name}, Balance: ${self.account_balance}")
             print(f"Amount in wallet: ${self.wallet}")
+            return self
 
     def displayUserbalance(self):
         print(f"{self.name}, Balance: ${self.account_balance}")
-
+        return self
 
 
     def make_withdraw(self, amount):
@@ -27,15 +29,18 @@ class User:
             self.wallet += amount
             print(f"{self.name}, Balance: ${self.account_balance}")
             print(f"Amount in wallet: ${self.wallet}")
+            return self
     
     def makeTransfer(self, recipient, amount):
         if amount > self.account_balance:
             print("insufficient funds in account balance!!")
+            return self
         else:
             self.account_balance -= amount
             recipient.account_balance += amount
             self.displayUserbalance()
             recipient.displayUserbalance()
+            return self
     
 
 arnold = User("Arnold Su", "a.su@gmail.com")
